c416ec3fd363f538009dde94ba34bdbdbabac522
jokLiu <liutkusjokubas@gmail.com>
2016-11-02 18:44:17 +0000

    finished

--- assessed/gcd.s
+++ assessed/gcd.s
@@ -1,38 +1,38 @@
     .data
     .align 2
-# Input the string
+# Input the strings
     input1:  	.asciiz "\nEnter first number: " 
     input2:  	.asciiz "\nEnter second number: "
     cr:	 	    .asciiz "\n"
     errmsg:	    .asciiz "\nError - must enter a positive integer\n"
     
-#standard preliminaries
+# Standard preliminaries
     .text
     .globl main
     
 main:
 
-#prompt input 1
+# Prompt input 1
 INPUT1:
     li 	    $v0,4	   	; # System call code 4 for print string
     la 	    $a0,input1  ; # Argument string as input
     syscall			    ; # Print the string
-    # Read first input 
+    # Read the first input 
     li 	    $v0,5	    ; # System call code 5 to read int input
     syscall			    ; # Read first input
-	move 	$a1,$v0	    ; # Move number into $a1 (m value from pseudocode)
+	move 	$a1,$v0	    ; # Move number into a1 register (m value from pseudocode)
 	bgtz	$a1,INPUT2	; # If valid input go to input 2 code, else get input 1 again
     li 	    $v0,4	    ; # System call code 4 for print string
     la 	    $a0,errmsg  ; # Error argument string as input
     syscall		    	; # Print the string
 	j       INPUT1	    ; # Get the input 1 again
 	
-#prompt input 2
+# Prompt input 2
 INPUT2:
     li 	    $v0,4	    ; # System call code 4 for print string
     la 	    $a0,input2	; # Argument string as input
     syscall			    ; # Print the string
-    # Read second input 
+    # Read the second input 
     li  	$v0,5		; # System call code 5 to read int input
     syscall			    ; # Read second input
 	move 	$a2,$v0	    ; # Move number into a2 (n value from pseudocode)
@@ -56,10 +56,10 @@ EXIT:
 	li  	$v0,10	    ; # System call code for exit
     syscall			    ; # Exit	
 
-#main greatest common divisor function	
+# Main greatest common divisor function	
 GCDMAIN:
-    li      $t0,0       ; # Load 0 into register t0
-    bne     $a2,$t0,REC ; # Check if n==0 then we return m else jump to recursion
+    li      $t0,0       ; # Load value 0 into register t0
+    bne     $a2,$t0,REC ; # Check if n!=0 then jump to recursion else we return value m
     move    $v0,$a1     ; # Copy m value from register a1 to function return register v0
     jr      $ra         ; # Return
 REC:
@@ -80,7 +80,7 @@ MOD:
 LOOP:                   ; # Start the while loop
     mul     $t2,$t1,$a2 ; # Multiply i*m where i is in register $t1 and m in register $a2
     sub     $t2,$a1,$t2 ; # Substract n-(i*m) where i*m is in register $t2 and n in register $a1
-    bltz    $t2,ENDLOOP ; # If value of n-(i*m) is less than zero we jump to return 
+    bltz    $t2,ENDLOOP ; # If value of n-(i*m) is less than zero we jump out of the loop to return the value
     addi    $t1,$t1,1   ; # Else we increment i by 1
     j       LOOP        ; # Repeat the loop
 ENDLOOP:                ; # End of the loop
